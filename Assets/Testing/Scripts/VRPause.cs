54a9528c6178e233794823fdc015e73dc9d52b31
jens <jensmonne@gmail.com>
2025-11-19 15:08:22 +0100

    Cleaned up the project following code convention

--- Assets/Testing/Scripts/VRPause.cs
+++ Assets/Testing/Scripts/VRPause.cs
@@ -1,12 +1,12 @@
+#if UNITY_EDITOR
 using UnityEngine;
 
-public class VRPauseEditor : MonoBehaviour
+public class VRPause : MonoBehaviour
 {
     public void PauseNow()
     {
-#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPaused = true;
         Debug.Log("Editor paused from VR button.");
-#endif
     }
 }
+#endif
