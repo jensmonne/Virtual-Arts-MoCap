48440e86213b8c50785e7bbd202aedd1f7cdcb7e
jens <jensmonne@gmail.com>
2025-12-05 16:01:48 +0100

    stuff

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] private GameObject door;
+    
     public void OpenDoor()
     {
-        
+        door.transform.localRotation = Quaternion.Euler(0, 42, 0);
     }
 
     public void CloseDoor()
     {
-        
+        door.transform.localRotation = Quaternion.Euler(0, -90, 0);
     }
 }
